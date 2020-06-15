6c4d7e5431faaa3d30ceea7c71c6886e9ca705a9
zm <411853835@qq.com>
2020-06-15 21:29:57 +0800

    components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="hello">
-    <h1 v-if="flag">{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['flag'],
   name: 'HelloWorld',
   data () {
     return {
