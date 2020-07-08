ad17fc02c3856165b0bde60d6e93972cf662a39b
zhaom <shs-zhcsyfczyzm@chinaunicom.cn>
2020-07-08 08:42:34 +0800

    more

---
+++ src/learn/props/Compa.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ msgCopy }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg'],
+  computed: {
+    msgCopy () {
+      return this.msg
+    }
+  }
+}
+</script>
---
+++ src/learn/props/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Compa :msg="msg" />
+    <Compb age="20" />
+    <button @click="change">change</button>
+  </div>
+</template>
+
+<script>
+import Compa from './Compa.vue'
+import Compb from './Compb.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: {
+        a: 1,
+        b: 2
+      }
+    }
+  },
+  components: {
+    Compa,
+    Compb
+  },
+  methods: {
+    change () {
+      this.msg.a = 2
+    }
+  }
+}
+</script>
